eb081f3a6ad21cb56b4eb2bbce73e81f72cb9732
Amiee <tjup02@gmail.com>
2026-04-18 22:07:38 +0800

    git add . git commit -m '改專案名稱(base)'

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-depoly-sample/',
+  // base: '/vite-deploy-sample/',
+  // -告訴 Vite：這個網站不是掛在網域根目錄
+  // -而是掛在 /vite-deploy-sample/ 底下
+  base: '/vite-deploy-sample/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
